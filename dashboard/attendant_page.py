35e418967e577772e03337b64dd1742f784b00e6
Chrystian H <chrystianh60@gmail.com>
2025-09-18 15:39:15 -0400

    feat/add buttons for commands

--- dashboard/attendant_page.py
+++ dashboard/attendant_page.py
@@ -1,6 +1,7 @@
 from tkinter import *
 master = Tk()
 master.geometry("500x500")
+master
 
 menu = Menu(master)
 master.config(menu=menu)
@@ -12,5 +13,12 @@ filemenu.add_command(label='Editar/Cancelar Consulta')
 filemenu.add_command(label="Listar Consultas")
 filemenu.add_command(label="Relatórios")
 filemenu.add_command(label="Sair", command=master.quit)
+
+Button(text="Cadastro de Paciente", width=25).grid(row=1, column=1)
+Button(text="Agendar Consulta", width=25).grid(row=2, column=1)
+Button(text="Editar/Cancelar Consultar", width=25).grid(row=3, column=1)
+Button(text="Listar Consultas", width=25).grid(row=5, column=1)
+Button(text="Relatórios", width=25).grid(row=6, column=1)
+
 
 master.mainloop()
